9f9375ba36abe952bfe12bcbfe0f58a31ecfc96e
余語久史 <yogohisashi@yogohisashinoMacBook-Pro.local>
2018-10-31 17:12:32 +0900

    map_reduce の練習だよーん

---
+++ jp/co/yogo/test55.py
@@ -0,0 +1,42 @@
+import datetime
+import collections as ct
+
+import more_itertools as mit
+
+data_i = [
+    {"date": datetime.date(2013, 1, 1), "id": 99, "value1": 12, "value2": 14},
+    {"date": datetime.date(2013, 1, 1), "id": 98, "value1": 13, "value2": 9},
+    {"date": datetime.date(2013, 1, 2), "id": 99, "value1": 14, "value2": 8}
+]
+
+
+#kfunc = lambda d: d["date"]
+#vfunc = lambda d: {k:v for k,v in d.items() if k.startswith("val")}
+#rfunc = lambda lst: sum((ct.Counter(d) for d in lst), ct.Counter())
+
+def kfunc(d):
+    # 集計のカテゴリを決めるみたいだな。。
+    d_val = d["date"]
+    #d_val = (d["date"],d["id"]) key複数の場合は tuple で渡してあげればok
+    return d_val
+
+def vfunc(d):
+    # 集計値の定義をするらしいいいいい。。
+    val = {k:v for k,v in d.items() if k.startswith("val")}
+    return val
+
+def rfunc(lst):
+    # reduce function 集約してくれるみだいだ。
+    r_val = sum((ct.Counter(d) for d in lst), ct.Counter())
+    return r_val
+
+
+result = mit.map_reduce(data_i, keyfunc=kfunc, valuefunc=vfunc, reducefunc=rfunc )
+
+for k,v in result.items():
+    print(k)
+    print(v)
+
+
+
+
